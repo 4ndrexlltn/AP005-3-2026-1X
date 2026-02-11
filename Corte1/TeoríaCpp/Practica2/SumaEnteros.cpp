6ac4732279795ca995c9cda25380dd8e04eb04d6
4ndrexlltn <felipellanteng16@gmail.com>
2026-02-11 15:24:39 -0500

    Add files via upload

---
+++ Corte1/TeoríaCpp/Practica2/SumaEnteros.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, suma;
+
+    // Solicitar al usuario que ingrese dos números
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    // Calcular la suma
+    suma = num1 + num2;
+
+    // Mostrar el resultado
+    cout << "La suma es: " << suma << endl;
+
+    return 0;
+}
