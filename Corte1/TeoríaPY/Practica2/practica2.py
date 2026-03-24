410c87b973e9d1c86e70c8a0e4858f22fde7d308
4ndrexlltn <felipellanteng16@gmail.com>
2026-03-23 20:52:06 -0500

    Create practica2.py

---
+++ Corte1/TeoríaPY/Practica2/practica2.py
@@ -0,0 +1,23 @@
+n1 = 3
+n2 = 3.0
+n3 = "3"
+
+print("n1 es: ")
+print(type(n1),'\n')
+print("n2 es: ")
+print(type(n2),'\n')
+print("n3 es: ")
+print(type(n3),'\n')
+print("--/--/--/--/--/--/\n")
+
+
+print("Primera forma de impresion en  una sola linea\n")
+print(f'n1 es: {type(n1)}')
+print(f'n2 es: {type(n2)}')
+print(f'n3 es: {type(n3)}\n')
+
+
+print("Segunda forma de impresion en  una sola linea\n")
+print('n1 es:',type(n1))
+print('n2 es:',type(n2))
+print('n3 es:',type(n3))
